feb4eb5a6290de5b418ef4346d2cafb4e98ffeba
thdmsdl97 <thdmsdl97@gmail.com>
2022-10-20 15:45:43 +0900

    장바구니 수정

--- cookWorkSpace/cookPrj/src/com/kh/cook/cart/CntCartController.java
+++ cookWorkSpace/cookPrj/src/com/kh/cook/cart/CntCartController.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/cart/delete")
+@WebServlet(urlPatterns = "")
 public class CntCartController extends HttpServlet {
 
 	@Override
